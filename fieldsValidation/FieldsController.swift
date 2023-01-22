a70e1feeff0292cc319e771ad0a43f5025b722ca
steellson <a.steellson@gmail.com>
2023-01-22 14:25:50 +0300

    Initial

---
+++ fieldsValidation/FieldsController.swift
@@ -0,0 +1,29 @@
+//
+//  FieldsController.swift
+//
+//  Created by Steellson
+
+
+import UIKit
+
+
+//MARK: - FieldsController
+
+final class FieldsController: UIViewController {
+    
+    
+    //MARK: - UI Elements
+    
+    
+    
+    
+//MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+
